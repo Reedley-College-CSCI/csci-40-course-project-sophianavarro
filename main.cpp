461983d03f662ec5767f775753c38efd6982ab14
sophianavarro <snavarr23@gmail.com>
2025-11-15 21:53:44 -0800

    Added taskPriority function

--- main.cpp
+++ main.cpp
@@ -8,16 +8,25 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 struct Task {
     string name;
-
+    int points;
+    int priority;
 };
 
 struct Reward {
-    string pointsNeeded;
+    string name;
+    int pointsNeeded;
 };
+
+string taskPriority(int priority) {
+    if (priority == 1) return "High";           // task priority that displays a string 
+    if (priority == 2) return "Medium";
+    return "Low";
+}
 
 int main() {
     return 0;
